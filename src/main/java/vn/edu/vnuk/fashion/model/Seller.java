889ae3765214982c630349749724e2f23492a5cb
lethithuhuong99 <huong.le170205@vnuk.edu.vn>
2019-05-08 10:23:53 +0700

    Model: color, length, pattern, priceType, seller, title

---
+++ src/main/java/vn/edu/vnuk/fashion/model/Seller.java
@@ -0,0 +1,47 @@
+package vn.edu.vnuk.fashion.model;
+
+import javax.validation.constraints.NotNull;
+
+public class Seller {
+	private int id;
+	@NotNull
+	private String label;
+	@NotNull
+	private String address;
+	@NotNull
+	private String phone;
+	@NotNull
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
