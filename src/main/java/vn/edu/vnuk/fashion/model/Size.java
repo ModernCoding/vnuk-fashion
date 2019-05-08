50b89cd88ee09ecab6b6c488ede0b30bbd5fa809
tnmquan9799 <tnmquan9799@gmail.com>
2019-05-08 10:14:14 +0700

    Models: BodyPart, Category, Gender, Size, Sleeve

---
+++ src/main/java/vn/edu/vnuk/fashion/model/Size.java
@@ -0,0 +1,70 @@
+package vn.edu.vnuk.fashion.model;
+
+public class Size {
+	private int id;
+	
+	private String universal;
+	private String us;
+	private String uk;
+	private String france;
+	private String italy;	
+	private String germany;
+	private String australia;
+	private String japan;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUniversal() {
+		return universal;
+	}
+	public void setUniversal(String universal) {
+		this.universal = universal;
+	}
+	public String getUs() {
+		return us;
+	}
+	public void setUs(String us) {
+		this.us = us;
+	}
+	public String getUk() {
+		return uk;
+	}
+	public void setUk(String uk) {
+		this.uk = uk;
+	}
+	public String getFrance() {
+		return france;
+	}
+	public void setFrance(String france) {
+		this.france = france;
+	}
+	public String getItaly() {
+		return italy;
+	}
+	public void setItaly(String italy) {
+		this.italy = italy;
+	}
+	public String getGermany() {
+		return germany;
+	}
+	public void setGermany(String germany) {
+		this.germany = germany;
+	}
+	public String getAustralia() {
+		return australia;
+	}
+	public void setAustralia(String australia) {
+		this.australia = australia;
+	}
+	public String getJapan() {
+		return japan;
+	}
+	public void setJapan(String japan) {
+		this.japan = japan;
+	}
+
+}
