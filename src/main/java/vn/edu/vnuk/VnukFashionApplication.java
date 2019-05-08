1aba4fc7c488f7663a5d5c5c7c1a1f296ac4171f
lethithuhuong99 <huong.le170205@vnuk.edu.vn>
2019-05-08 08:31:08 +0700

    c

---
+++ src/main/java/vn/edu/vnuk/VnukFashionApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.vnuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnukFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnukFashionApplication.class, args);
+	}
+
+}
