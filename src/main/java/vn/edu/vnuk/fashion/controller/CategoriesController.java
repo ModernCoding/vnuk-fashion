5102c9a70ce7035de33391b769f6128f00db7ccb
Modern Coding <cormorant.programming@gmail.com>
2019-05-13 08:53:32 +0700

    CategoryController

---
+++ src/main/java/vn/edu/vnuk/fashion/controller/CategoriesController.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.edu.vnuk.fashion.controller;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import vn.edu.vnuk.fashion.dao.CategoryDao;
+import vn.edu.vnuk.fashion.model.Category;
+
+/**
+ *
+ * @author michel
+ */
+@Controller
+public class CategoriesController {
+	
+	private CategoryDao dao;
+	
+	@Autowired
+	public void setcategoryDao(CategoryDao dao) {
+		this.dao = dao;
+	}
+	
+	@RequestMapping(value={"", "/"})
+	public String home() {
+		return "redirect:/categories";
+	}
+    
+	
+	@RequestMapping("/categories")
+    public String index(Model model, ServletRequest request) throws SQLException{
+        model.addAttribute("categories", dao.read());
+        model.addAttribute("template", "category/index");
+        return "_layout";
+    }
+    
+    
+    @RequestMapping("/categories/{id}")
+    public String show(@PathVariable("id") Long id, Model model, ServletRequest request) throws SQLException{
+        model.addAttribute("category", dao.read(id));
+        model.addAttribute("template", "category/show");
+        return "_layout";
+    }
+    
+    
+    @RequestMapping("/categories/new")
+    public String add(Category category, Model model, @ModelAttribute("fieldErrors") ArrayList<FieldError> fieldErrors){
+    	
+    	for(FieldError fieldError : fieldErrors) {
+    		model.addAttribute(
+    				String.format("%sFieldError", fieldError.getField()),
+    				fieldError.getDefaultMessage()
+    			);
+    	}
+    	
+        model.addAttribute("template", "category/new");
+        return "_layout";
+    }
+    
+    
+    @RequestMapping("/categories/{id}/edit")
+    public String edit(
+    		
+		@RequestParam(value="backToShow", defaultValue="false") Boolean backToShow,
+		@PathVariable("id") Long id,
+		Category category,
+		Model model,
+		ServletRequest request,
+		@ModelAttribute("fieldErrors") ArrayList<FieldError> fieldErrors
+		
+	) throws SQLException{
+    	
+    	
+    	category = dao.read(id);
+    	
+    	for(FieldError fieldError : fieldErrors) {
+    		model.addAttribute(
+    				String.format("%sFieldError", fieldError.getField()),
+    				fieldError.getDefaultMessage()
+    			);
+    	}
+    	
+    	
+    	model.addAttribute("backToShow", backToShow);
+    	model.addAttribute("urlCompletion", backToShow ? String.format("/%s", id) : "");
+    	model.addAttribute("category", category);
+        model.addAttribute("template", "category/edit");
+
+        return "_layout";
+    
+        
+    }
+    
+    
+    @RequestMapping(value="/categories", method=RequestMethod.POST)
+    public String create(
+		
+    	@Valid Category category,
+    	BindingResult bindingResult,
+    	ServletRequest request,
+    	RedirectAttributes redirectAttributes
+    
+    ) throws SQLException{
+        
+    	
+        if (bindingResult.hasErrors()) {
+        	redirectAttributes.addFlashAttribute("fieldErrors", bindingResult.getAllErrors());
+            return "redirect:/categories/new";
+        }
+        
+        dao.create(category);
+        return "redirect:/categories";
+        
+        
+    }
+    
+    
+    @RequestMapping(value="/categories/{id}", method=RequestMethod.PATCH)
+    public String update(
+    		
+    		@RequestParam(value="backToShow", defaultValue="false") Boolean backToShow,
+    		@PathVariable("id") Long id,
+    		@Valid Category category,
+    		BindingResult bindingResult,
+    		ServletRequest request,
+    		RedirectAttributes redirectAttributes
+    		
+    	) throws SQLException{
+    	
+        
+    	if (bindingResult.hasErrors()) {
+        	redirectAttributes.addFlashAttribute("fieldErrors", bindingResult.getAllErrors());
+            return String.format("redirect:/categories/%s/edit", id);
+        }
+        
+        dao.update(category);
+        return backToShow ? String.format("redirect:/categories/%s", id) : "redirect:/categories";
+        
+        
+    }
+    
+    
+    //  delete with ajax
+    @RequestMapping(value="/categories/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") Long id, ServletRequest request, HttpServletResponse response) throws SQLException {
+    	dao.delete(id);
+        response.setStatus(200);
+    }
+    
+}
