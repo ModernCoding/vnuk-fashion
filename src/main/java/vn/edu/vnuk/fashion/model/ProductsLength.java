d7e44b9fe7786693022c9b035eb3e9f98c0d0953
tnmquan9799 <tnmquan9799@gmail.com>
2019-05-15 10:52:17 +0700

    label fixes

--- src/main/java/vn/edu/vnuk/fashion/model/ProductsLength.java
+++ src/main/java/vn/edu/vnuk/fashion/model/ProductsLength.java
@@ -4,10 +4,14 @@ import javax.validation.constraints.NotNull;
 
 public class ProductsLength {
 	private Long id;
+
+	@NotNull
+	private Long productId;
 	
 	@NotNull
-	private Product product;
+	private Long lengthId;
 	
+	private Product product;
 	private Length length;
 
 	public Long getId() {
@@ -33,5 +37,22 @@ public class ProductsLength {
 	public void setLength(Length length) {
 		this.length = length;
 	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public Long getLengthId() {
+		return lengthId;
+	}
+
+	public void setLengthId(Long lengthId) {
+		this.lengthId = lengthId;
+	}
+	
 	
 }
